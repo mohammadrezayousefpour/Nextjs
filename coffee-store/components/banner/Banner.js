28abe0e43290ca41b9dc18bd0a76204569a9895b
unknown <mohammad0reza79@gmail.com>
2022-05-10 12:51:26 +0430

    picture added

--- coffee-store/components/banner/Banner.js
+++ coffee-store/components/banner/Banner.js
@@ -9,9 +9,11 @@ const banner = (props) => {
         <span className={styles.title2}>Connoisseur</span>
       </h1>
       <p className={styles.subTitle}>Discover your local coffee shops!</p>
-      <button className={styles.button} onClick={props.handleOnclick}>
-        {props.buttonText}
-      </button>
+      <div className={styles.buttonWrapper}>
+        <button className={styles.button} onClick={props.handleOnclick}>
+          {props.buttonText}
+        </button>
+      </div>
     </div>
   );
 };
--- coffee-store/pages/index.js
+++ coffee-store/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Banner from "../components/banner/Banner";
+import Image from "next/image";
 
 export default function Home() {
   const handleOnBannerBtnClick = () => {
@@ -17,6 +18,11 @@ export default function Home() {
           buttonText="View Stores nearby"
           handleOnclick={handleOnBannerBtnClick}
         />
+        <Image
+          src="/static/3.1 hero-image.png"
+          width={700}
+          height={400}
+        ></Image>
       </main>
     </div>
   );
