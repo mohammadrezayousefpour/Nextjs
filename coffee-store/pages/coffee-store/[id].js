93c2d28cb63ffaec0ab4e9cc3228df95f4189819
unknown <mohammad0reza79@gmail.com>
2022-05-10 12:33:37 +0430

    link and a tag checked and useRouter added

--- coffee-store/pages/coffee-store/[id].js
+++ coffee-store/pages/coffee-store/[id].js
@@ -1,10 +1,23 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 
 const CoffeeStore = () => {
   const router = useRouter();
   console.log(router);
-  return <div>this is CoffeeStore page : {router?.query.id}</div>;
+  return (
+    <div>
+      <span>is CoffeeStore page : {router?.query.id}</span>
+      <button>
+        <a href="/">Back To Home with a tag</a>
+      </button>
+      <button>
+        <Link scroll={true} href="/">
+          <a>Back To Home with link tag</a>
+        </Link>
+      </button>
+    </div>
+  );
 };
 
 export default CoffeeStore;
