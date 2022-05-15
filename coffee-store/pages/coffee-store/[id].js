eacc4556fc8dcc66de50885cb4e0a0935299985f
unknown <mohammad0reza79@gmail.com>
2022-05-15 08:27:03 +0430

    next ended

--- coffee-store/pages/coffee-store/[id].js
+++ coffee-store/pages/coffee-store/[id].js
@@ -1,7 +1,11 @@
+import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 import coffeeStoresData from "../../data/coffee-stores.json";
+import classes from "../../styles/coffee-stores.module.css";
+import Image from "next/image";
+import cls from "classnames";
 
 export function getStaticProps(props) {
   const params = props.params;
@@ -16,15 +20,23 @@ export function getStaticProps(props) {
 }
 
 export function getStaticPaths() {
+  const paths = coffeeStoresData.map((coffee) => {
+    return {
+      params: {
+        id: coffee.id.toString(),
+      },
+    };
+  });
+
   return {
-    paths: [
-      // { params: { id: "0" } },
-      // { params: { id: "1" } },
-      // { params: { id: "300" } },
-    ],
+    paths: paths,
     fallback: true,
   };
 }
+
+const handleUpvoteButton = () => {
+  console.log("salam");
+};
 
 const CoffeeStore = (props) => {
   const router = useRouter();
@@ -32,15 +44,47 @@ const CoffeeStore = (props) => {
   if (router.isFallback) {
     return <div>Loading</div>;
   }
+
   return (
-    <div>
-      <span>is CoffeeStore page : {router?.query.id}</span>
-      <a href="/">Back To Home with a tag</a>
-      <Link scroll={true} href="/">
-        <a>Back To Home with link tag</a>
-      </Link>
-      <p>{props.coffeeStore.name}</p>
-      <p>{props.coffeeStore.address}</p>
+    <div className={classes.layout}>
+      <Head>
+        <title>{props.coffeeStore.name}</title>
+      </Head>
+      <div className={classes.container}>
+        <div className={classes.col1}>
+          <div className={classes.backToHomeLink}>
+            <Link href="/">
+              <a>Back To home</a>
+            </Link>
+          </div>
+          <div className={classes.nameWrapper}></div>
+          <h1 className={classes.name}>{props.coffeeStore.name}</h1>
+          <Image
+            src={props.coffeeStore.imgUrl}
+            width={600}
+            height={360}
+            className={classes.storeImage}
+            alt={props.coffeeStore.name}
+          ></Image>
+        </div>
+        <div className={cls("glass", classes.col2)}>
+          <div className={classes.iconWrapper}>
+            <Image src="/static/icons/push.svg" width={24} height={24}></Image>
+            <p className={classes.text}>{props.coffeeStore.address}</p>
+          </div>
+          <div className={classes.iconWrapper}>
+            <Image src="/static/icons/place.svg" width={24} height={24}></Image>
+            <p className={classes.text}>{props.coffeeStore.neighbourhood}</p>
+          </div>
+          <div className={classes.iconWrapper}>
+            <Image src="/static/icons/start.svg" width={24} height={24}></Image>
+            <p className={classes.text}>5</p>
+          </div>
+          <button className={classes.upVoteButton} onClick={handleUpvoteButton}>
+            Up vote
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
